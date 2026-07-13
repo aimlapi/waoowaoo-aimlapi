bbed8591c653b8a56b1e4b442a5eb08f71f1318f
saturn <saturndeccc@gmail.com>
2026-07-13 22:09:07 +0800

    refactor(ui): remove unused glass primitives
    Delete six primitives that were only re-exported by the barrel and narrow that barrel to the three components with real consumers.\n\nValidation: npm run typecheck; npm run test:fast (549 passed, 0 skipped).

--- src/components/ui/primitives/index.ts
+++ src/components/ui/primitives/index.ts
@@ -1,24 +1,4 @@
-export { default as GlassSurface } from './GlassSurface'
-export type { GlassSurfaceProps, UiDensity } from './GlassSurface'
-
-export { default as GlassButton } from './GlassButton'
-export type { GlassButtonProps } from './GlassButton'
-
-export { default as GlassField } from './GlassField'
-export type { GlassFieldProps } from './GlassField'
-
-export { default as GlassInput } from './GlassInput'
-export type { GlassInputProps } from './GlassInput'
 export { default as GlassNumberStepper } from './GlassNumberStepper'
 export type { GlassNumberStepperProps } from './GlassNumberStepper'
 export { default as GlassSelect } from './GlassSelect'
 export type { GlassSelectOption, GlassSelectProps } from './GlassSelect'
-
-export { default as GlassTextarea } from './GlassTextarea'
-export type { GlassTextareaProps } from './GlassTextarea'
-
-export { default as GlassChip } from './GlassChip'
-export type { GlassChipProps, UiTone } from './GlassChip'
-
-export { default as GlassModalShell } from './GlassModalShell'
-export type { GlassModalShellProps } from './GlassModalShell'
